2ce8f811c24321a8b0aa910955babaa328c4b09b
CL-YQLM <145878564+CL-YQLM@users.noreply.github.com>
2026-06-21 22:17:09 +0800

    tune ADC sampling params and update config
    - halve supersample_cnt (16 → 8) and double discard_rounds (2 → 4)
    - update serial port to COM3 in WiSensConfig
    - enable IDF_EXPERIMENTAL_FEATURES in sdkconfig

--- internal_adc/firmware/main/main.c
+++ internal_adc/firmware/main/main.c
@@ -1,5 +1,6 @@
 #include "decoder.h"
 #include "esp_err.h"
+#include "freertos/FreeRTOS.h"
 #include "pin_defs.h"
 #include "sense.h"
 #include "soc/soc_caps.h"
@@ -7,14 +8,18 @@
 #include <stdint.h>
 
 void app_main(void) {
+    // while (true) {
+    //     printf("hello\n");
+    //     vTaskDelay(pdMS_TO_TICKS(1000));
+    // }
     static uint16_t frame_buf[NUM_FSR_DRIVES * NUM_FSR_SENSES];
 
     fsr_drive_decoder_init();
     fsr_sense_init((fsr_sense_cfg_t){
-        .supersample_cnt = 16,
+        .supersample_cnt = 8,
         .bit_width = SOC_ADC_DIGI_MAX_BITWIDTH,
         .sample_freq_hz = 80'000,
-        .discard_rounds = 2,
+        .discard_rounds = 4,
     });
 
     fsr_wiresens_cfg_t wiresens_cfg =
